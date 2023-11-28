cd28254882d218577d079f2a9cfc3fb311da8781
VATSAL Panchani <reactdeveloper3010@gmail.com>
2023-11-28 18:36:19 +0530

    \n and \t use

---
+++ Program1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+main(){
+	//1
+	printf("My name is vatsal \n");
+	printf("Age : 20 \n");
+	printf("My School name is Taravada Gurukul \n");
+	printf("My College name is BMU \n\n"); 
+	
+	printf("************************ \n");
+	
+	//2
+ 	printf("- - - - - - - -\n");
+    printf("|\t\t|\n");
+    printf("R\t\t|\n");
+    printf("N\t\t|\n");
+    printf("W\t\t|\n");
+    printf("|\t\t|\n");
+    printf("- - - - - - - -\n\n\n");
+    printf("************************ \n\n\n\n");
+    //3
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("**\n");
+    printf("*\n\n");
+    
+    printf("************************ \n\n\n\n");
+    //4
+     printf("*\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t*\t\t\t\t*\t\t\t\t\t\t\t*\n");
+    printf("*\t\t*\t\t\t*\t\t\t*\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n");
+    printf("*\t\t\t\t\t\t\t\t\t\t\t\t*\n");
+    
+    return 0;
+}
